d816597497a9d146ee8e22999c0a5fb4185bf726
Hayden Betts <haydenbetts@gmail.com>
2017-09-06 18:45:58 +0000

    easy owner methods passing

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name
+
+  @pets = []
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all=[]
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def initialize(species)
+    @species = species
+    self.class.all << self
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    puts "I am a #{self.species}."
+  end
+
 end
